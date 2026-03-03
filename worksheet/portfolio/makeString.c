89b447779f04e986260dee7ca89c5b55a70c3c98
Xavier Cunningham <zvnz0038@leeds.ac.uk>
2026-03-03 11:38:43 +0000

    yo

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Xavier Cunningham
+Student ID: 202028992
 */
 
 #include <stdio.h>
@@ -9,6 +9,15 @@ Student ID:
 
 int main( int argc, char **argv ) {
 
+    char buffer[64] = "";
+
+    for (int i = 1; i < argc; i++) { //starts at 1 due to 0 being program name
+        strcat(buffer, argv[i]); 
+        if (i < argc - 1) { //add hyphon unless last argument
+            strcat(buffer, "-"); 
+            }       
+
+    }
     // define appropriate data to hold your answer
 
     // process the command-line data using appropriate string functions
